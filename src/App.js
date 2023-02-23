329363546de364ff7f4ddba45eb4190ec4d19357
maxhannan <maxhannan@substanceseven.com>
2023-02-22 18:16:43 -0600

    counter

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-
-import './App.css';
-import Component from './component';
-
-function App() {
+import { useState } from "react"
+const App = () => {
+ const [counter, setCounter] = useState(0)
   return (
-    <div>
-         <Component />
+    <div style ={{width: '100%', height: '100%'}}>
+    <div style ={{fontSize: "25pt", textAlign: 'center'}}>
+    <h1>{counter}</h1>
+    <button style ={{fontSize: "25pt"}} onClick={()=> setCounter(counter + 1)}>+</button>
+   <button style ={{fontSize: "25pt"}} onClick={()=> setCounter(0)}>reset</button>
+   <button style ={{ fontSize: "25pt"}} onClick={()=> setCounter(counter - 1)}>-</button>
+   
     </div>
-  );
+    </div>
+  )
+  
 }
 
-export default App;
+export default App
